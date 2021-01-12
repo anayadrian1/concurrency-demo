df3c61e0fb358b9aa4ba6d7e1ece2742d2c9d694
Adrian Anaya <aanaya79@cnm.edu>
2021-01-12 11:38:43 -0700

    Threading examples

---
+++ src/edu/cnm/deepdive/processing/MultiThreadedWithCriticalSection.java
@@ -0,0 +1,45 @@
+package edu.cnm.deepdive.processing;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MultiThreadedWithCriticalSection extends DataProcessor {
+
+  private static final int NUM_TASKS = Runtime.getRuntime().availableProcessors();
+
+  private final Object lock = new Object();
+
+  private double logSum;
+
+  @Override
+  public void process() throws InterruptedException {
+    logSum = 0;
+    int[] data = getData();
+    int length = data.length;
+    ExecutorService pool = Executors.newFixedThreadPool(NUM_TASKS);
+    CountDownLatch latch = new CountDownLatch(NUM_TASKS);
+    for (int i = 0; i < NUM_TASKS; i++) {
+      int start = i * length / NUM_TASKS;
+      int end = (i + 1) * length / NUM_TASKS;
+      pool.submit(() -> {
+        for (int j = start; j < end; j++) {
+          double logValue = Math.log(data[j]);
+          // logSum is the shared memory that all threads have access too
+          // synchronize with a final Object i.e. lock
+          synchronized (lock) {
+            logSum += logValue;
+          }
+        }
+        latch.countDown();
+      });
+    }
+    latch.await();
+    pool.shutdown();
+  }
+
+  @Override
+  public double getGeometricMean() {
+    return Math.exp(logSum / getData().length);
+  }
+}
